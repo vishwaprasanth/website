8b5df87652c1e0588562ac8d3ecf33993dc92d2f
vishwaprasanth <vishwaprasanth1411@gmail.com>
2023-04-11 17:45:30 +0530

    intro page

---
+++ src/Component/about/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../../Component/about/about.css";
+
+const about = () => {
+  return (
+    <div className='a'>
+        <div className='a-left'>
+            <div className='a-card-bg'></div>
+            <div className='a-card'>
+                <img className='a-img' src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+            </div>
+        </div>
+        <div className='a-right'></div>
+    </div>
+  )
+}
+
+export default about
